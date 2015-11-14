634f6338a84abe0d10ca2377c9f2fef256cb3b45
Phil Fishbein <phil@fishbe.in>
2015-11-14 11:36:41 -0500

    Updated to use urban-dict-py library

--- getword.py
+++ getword.py
@@ -1,5 +1,4 @@
 from lxml import html
 import requests
-page = requests.get("http://urbandictionary.com/random.php")
-tree = html.fromstring(page.content)
-print (tree.xpath('//a[@class="word"]/text()')[0])
+from urban import *
+print random()[0][0] + ": " + random()[0][1]
